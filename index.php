da2cf014adbc769685730218292d83208bd189b5
Flaimeur <131146989+Flaimeur@users.noreply.github.com>
2025-11-28 12:08:18 +0100

    Update order button markup on homepage
    Simplified the 'Passer une commande' button by removing the surrounding div and added a 'herf' attribute pointing to 'Passer commande.php'.

--- index.php
+++ index.php
@@ -11,10 +11,7 @@
     <h1 class="titre-2">Page d'Accueil</h1>
 
     <div class="container">
-        <div>
-            <button class="bouton-commande">Passer une commande</button> <br>
-        </div>
-        
+        <button herf="Passer commande.php" class="bouton-commande">Passer une commande</button> <br>        
         <button class="bouton-acces-compte">Accéder à votre compte</button> <br>
         <button class="bouton-incription">Incrivez vous et découvrez les avanteges de la carte fidélité</button> <br>
         <button class="bouton-gestion-données">Gestion de la base de données</button> <br>
